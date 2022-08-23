be7e4ccbd4b01b0ce6fb00938d08c546b120164b
harshita <harshita1225@gmail.com>
2022-08-23 08:27:52 +0200

    task completed

--- index.js
+++ index.js
@@ -11,6 +11,20 @@ function fibonacci(nums) {
   return data;
 }
 
+let fibonacci2 = function (n) {
+  if (n === 0) {
+    return [0];
+  } else if (n === 1) {
+    return [0, 1];
+  } else {
+    let s = fibonacci2(n - 1);
+    s.push(s[s.length - 1] + s[s.length - 2]);
+    return s;
+  }
+};
+
+console.log(fibonacci2(5));
+
 console.log(fibonacci(0)); // [0]
 console.log(`----------------`);
 console.log(fibonacci(1)); // [ 0, 1 ]
